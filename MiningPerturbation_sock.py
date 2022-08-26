a5c227be4d5bf87d458a4917b1b054e2bcc3256b
LEE KIHO <47383452+lekiho@users.noreply.github.com>
2022-08-26 10:03:58 -0400

    Add files via upload

--- MiningPerturbation_sock.py
+++ MiningPerturbation_sock.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from scapy.all import *
 from random import randint
 
@@ -42,63 +43,43 @@ def receive_from(connection):
         pass
         return buffer
 
-def request_handler(buffer):
+def request_handler(local_buffer, remote_host, remote_port):
     # perform packet modifications
     
-    return buffer
+    class Request(object):
+        def __init__(self, verb, *args):
+            self.verb = verb
+            self.args = [str(x) for x in args]
+    
+    # dummy packet
+    # send(IP(dst = remote_host)/ICMP())
+    # send(Ether()/IP(dst = local_buffer.getsockname()[0], ttl =(1,1)), iface="eth0")
+    send(IP(dst = remote_host, ttl = (1,1))/TCP(flags = "S", sport = RandShort(), dport = remote_port)/Raw(""))
+
+    # splitting 
+    fragsize = 500
+    # bufsize = local_buffer.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
+    buffsize = len(local_buffer)
+    fragsize = (fragsize + 7) // 8 * 8
+    
+    if(buffsize > fragsize):
+        print(local_buffer)
+        
+        s = [local_buffer.decode("utf-8")[i:i+1] for i in range(0, len(local_buffer), 1)]
+
+        if len(s) == 2:
+            req_str = s[0]
+            local_buffer = s[1]
+            req_lst = [req_str[j:j+1] for j in range(0, len(req_lst), 1)]
+            local_buffer = Request(req_lst[0], *req_lst[1:])
+        # data = local_buffer
+        # local_buffer += data
+    return local_buffer
 
 def response_handler(buffer):
     # perform packet modifications
     
     return buffer
-
-def fragment(remote_host, buffer, fragsize=1480):
-    # Fragment a big IP datagram
-    frag_host = remote_host
-    fragsize = (fragsize + 7) // 8 * 8
-    lst = []
-    for p in buffer:
-        s = raw(p[frag_host].payload)
-        nb = (len(s) + fragsize - 1) // fragsize
-
-        for i in range(nb):
-            q = p.copy()
-            del(q[frag_host].payload)
-            del(q[frag_host].chksum)
-            del(q[frag_host].len)
-
-            if i != nb - 1:
-                q[frag_host].flags |= 1
-            q[frag_host].frag += i * fragsize // 4
-            r = conf.raw_layer(load=s[i * fragsize:(i + 1) * fragsize])
-            r.overload_fields = p[frag_host].payload.overload_fields.copy()
-            q.add_payload(r)
-            lst.append(q)
-    return lst
-
-def callback_splitting(local_buffer, fragsize = 1480):
-    data = local_buffer.get_data()
-
-    fragsize = (fragsize + 7) // 8 * 8
-    lst = []
-    for p in data:
-        s = raw(p[IP].payload)
-        nb = (len(s) + fragsize - 1) // fragsize
-
-        for i in range(nb):
-            q = p.copy()
-            del(q[IP].payload)
-            del(q[IP].chksum)
-            del(q[IP].len)
-
-            if i != nb - 1:
-                q[IP].flags |= 1
-            q[IP].frag += i * fragsize // 4
-            r = conf.raw_layer(load=s[i * fragsize:(i + 1) * fragsize])
-            r.overload_fields = p[IP].payload.overload_fields.copy()
-            q.add_payload(r)
-            lst.append(q)
-    return lst
 
 def proxy_handler(client_socket, remote_host, remote_port, receive_first):
     remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -121,25 +102,11 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
             print(line)
             hexdump(local_buffer)
             
-            # dummy packet
-            # send(IP(dst = dum_host)/ICMP())
-            # send(Ether()/IP(dst = local_buffer.getsockname()[0], ttl =(1,1)), iface="eth0")
-            send(IP(dst = remote_host)/TCP(flags = "S", sport = RandShort(), dport = remote_port)/Raw(""))
-
-            # splitting
-            
             local_buffer_list = []
-            try:
-                local_buffer_list = callback_splitting(local_buffer, 500)
-
-            except KeyboardInterrupt:
-                print('')
-
-            local_buffer = request_handler(local_buffer)
-
-            for i in range(len(local_buffer_list)):
-                remote_socket.send(local_buffer_list[i])    
-            print("[==>] Sent to remote "+ len(local_buffer_list) + " splitted packets.")
+            local_buffer_list = request_handler(local_buffer, remote_host, remote_port)
+            
+            remote_socket.send(bytes(local_buffer_list))    
+            print("[==>] Sent to remote %d bytes." % len(local_buffer_list))
 
         remote_buffer = receive_from(remote_socket)
         if len(remote_buffer):
@@ -161,12 +128,11 @@ def print_and_accept(pkt):
     pkt.accept()
 
 def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
-    
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
         server.bind((local_host, local_port))
-
+    
     except Exception as e:
         print('problem on bind: %r' % e)
 
@@ -176,12 +142,15 @@ def server_loop(local_host, local_port, remote_host, remote_port, receive_first)
     
     print("[*] Listening on %s:%d" % (local_host, local_port))
     server.listen(30)
-
+    # nfqueue.listen(30)
+    
     while True:
         client_socket, addr = server.accept()
+        
 
         #print out the local connection information
         line = "> Received incoming connection from %s:%d" % (addr[0], addr[1])
+    
         print(line)
 
         #start a thread to talk to the remote hosst
@@ -189,6 +158,7 @@ def server_loop(local_host, local_port, remote_host, remote_port, receive_first)
             target = proxy_handler,
             args=(client_socket, remote_host, remote_port, receive_first))
         proxy_thread.start()
+
 
 def main():
     if len(sys.argv[1:]) != 5:
@@ -210,16 +180,17 @@ def main():
     else:
         receive_first = False
     
-    # try:
+    try:
         server_loop(local_host, local_port, remote_host, remote_port, receive_first)
 
-    # except KeyboardInterrupt:
-    #     print("Flushing iptables")
-    #     os.system('iptables -F')
-    #     os.system('iptables -X')
+    except KeyboardInterrupt:
+        print('')
+        # print("Flushing iptables")
+        # os.system('iptables -F')
+        # os.system('iptables -X')
 
 
-# iptables = "iptables -I INPUT -d 192.168.126.0/24 -j NFQUEUE --queue-num 1"
+# iptables = "iptables -I INPUT -d 192.168.0.0/24 -j NFQUEUE --queue-num 1"
 # print("Adding iptable rules : ")
 # print(iptables)
 # os.system(iptables)
